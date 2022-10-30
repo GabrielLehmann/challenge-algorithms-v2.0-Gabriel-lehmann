2c536af001049274cf2bd358163f527a8cb1d592
Gabriel Lehmann <gabriel.lehmann@hotmail.com>
2022-10-29 23:13:17 -0300

    feat(fibonacci): made fourth challenge

--- 04-fibonacci/index.js
+++ 04-fibonacci/index.js
@@ -1,4 +1,12 @@
 export function fibonacci(value) {
   // implementar logica aqui
-  
+  let n1 = 0
+  let n2 = 1
+  let temp
+  for (let i = 0; i < value; i++) {
+    temp = n1
+    n1 = n1 + n2
+    n2 = temp
+  }
+  return n1
 }
