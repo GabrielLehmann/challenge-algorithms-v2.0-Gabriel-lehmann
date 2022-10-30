ece207425d685860fb553a7875266d4edbd6c7ed
Gabriel Lehmann <gabriel.lehmann@hotmail.com>
2022-10-29 23:42:52 -0300

    feat(mostrepeatedchar): made ninth challenge

--- 09-mostRepeatedChar/index.js
+++ 09-mostRepeatedChar/index.js
@@ -1,4 +1,18 @@
 export function mostUsedChar(text) {
     // implementar logica aqui
-    return ""
+    let count = 0;
+    let char = " ";
+    let save = {};
+    text.split("").forEach((letter) => {
+        if(save[letter]){
+            save[letter] += 1;
+        } else {
+            save[letter] = 1;
+        }
+        if(save[letter] > count){
+            count = save[letter];
+            char = letter;
+        }
+    })
+    return char;
 }
