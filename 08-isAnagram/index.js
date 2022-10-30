0fbf4632876dd0563d5eab81e43a4683f66aa6df
Gabriel Lehmann <gabriel.lehmann@hotmail.com>
2022-10-29 23:35:13 -0300

    feat(isanagram): made eighth challenge

--- 08-isAnagram/index.js
+++ 08-isAnagram/index.js
@@ -1,4 +1,4 @@
 export function isAnagram(word1, word2) {
   // implementar logica aqui
-  
+  return word1.toLowerCase().split("").sort().join("") == word2.toLowerCase().split("").sort().join("") ;
 }
