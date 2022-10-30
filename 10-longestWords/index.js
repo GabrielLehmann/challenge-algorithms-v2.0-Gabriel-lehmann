1036664d4c0fc4f3b3f1731f574ffa9c09b1fc2f
Gabriel Lehmann <gabriel.lehmann@hotmail.com>
2022-10-30 01:20:58 -0300

    feat(longestwords): made tenth challenge

--- 10-longestWords/index.js
+++ 10-longestWords/index.js
@@ -1,4 +1,9 @@
 export function longestWords(words) {
     // implementar logica aqui
-    
+    return words.reduce((longWords, currentWord) => {
+        if(!longWords[0] || longWords[0].length < currentWord.length) return [currentWord];
+        if(longWords[0].length > currentWord.length) return longWords;
+        longWords.push(currentWord);
+        return longWords;
+    },[])
 }
